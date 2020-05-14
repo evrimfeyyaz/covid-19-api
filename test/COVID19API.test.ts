e0aa08cdc28a03eeacaad7e33686b85cae88e0ee
Evrim P <evrimfeyyaz@gmail.com>
2020-05-14 14:16:10 +0400

    Mark the API class tests as 'todo'

--- test/COVID19API.test.ts
+++ test/COVID19API.test.ts
@@ -1,49 +1,49 @@
 describe('COVID19API', () => {
   describe('when initialized', () => {
     describe('getDataByLocation(s)', () => {
-      it('returns the data with added calculated information for a given global location');
-      it('returns the data with added calculated information for a given US county');
-      it('throws an error when the location cannot be found');
+      it.todo('returns the data with added calculated information for a given global location');
+      it.todo('returns the data with added calculated information for a given US county');
+      it.todo('throws an error when the location cannot be found');
     });
 
     describe('getDataByLocationAndDate', () => {
-      it('returns the data with added calculated values for the given location and date');
-      it('throws an error when no data exists for the given date');
+      it.todo('returns the data with added calculated values for the given location and date');
+      it.todo('throws an error when no data exists for the given date');
     });
 
     describe('locations', () => {
-      it('returns a list of all locations');
+      it.todo('returns a list of all locations');
     });
 
     describe('lastUpdatedAt', () => {
-      it('returns the date that the source data was last updated');
+      it.todo('returns the date that the source data was last updated');
     });
 
     describe('firstDate', () => {
-      it('returns the first date of the time series');
+      it.todo('returns the first date of the time series');
     });
 
     describe('lastDate', () => {
-      it('returns the last date of the time series');
+      it.todo('returns the last date of the time series');
     });
 
     describe('with lazy loading on', () => {
-      it('does not load the US state and county data before it is requested');
-      it('loads the US state and county data when it is requested');
-      it(
+      it.todo('does not load the US state and county data before it is requested');
+      it.todo('loads the US state and county data when it is requested');
+      it.todo(
         'includes all the US state and county names in the locations list even before the US data is requested'
       );
     });
 
     describe('with lazy loading off', () => {
-      it('loads the US state and county before it is requested');
+      it.todo('loads the US state and county before it is requested');
     });
 
-    it('does not reload the data when it is not expired');
-    it('reloads the data when it is expired');
+    it.todo('does not reload the data when it is not expired');
+    it.todo('reloads the data when it is expired');
   });
 
   describe('when not initialized', () => {
-    it('throws an error when one of its getters is called');
+    it.todo('throws an error when one of its getters is called');
   });
 });
