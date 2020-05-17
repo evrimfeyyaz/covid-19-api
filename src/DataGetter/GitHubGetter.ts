48135d2b974b6bcf0fac1cdfa4cc0ae5a2528515
Evrim P <evrimfeyyaz@gmail.com>
2020-05-17 12:08:18 +0400

    Remove GitHubGetter specific errors, and use DataGetterError instead

--- src/DataGetter/GitHubGetter.ts
+++ src/DataGetter/GitHubGetter.ts
@@ -1,33 +1,6 @@
 import { DataGetter, DataGetterError } from 'DataGetter/DataGetter';
 
 /**
- * Thrown when there is an error fetching the CSV time series files from the GitHub repository.
- */
-export class GitHubDataFetchError extends DataGetterError {
-  constructor(status: number, statusText: string) {
-    super(
-      `There was an error fetching the data from GitHub. Response status: ${status} - ${statusText}`
-    );
-    this.name = 'GitHubDataFetchError';
-    Object.setPrototypeOf(this, GitHubDataFetchError.prototype);
-  }
-}
-
-/**
- * Thrown when there is an error fetching the last commit date of the GitHub repository folder
- * containing the time series data.
- */
-export class GitHubCommitFetchError extends DataGetterError {
-  constructor(status: number, statusText: string) {
-    super(
-      `There was an error fetching the commit date from the GitHub API. Response status: ${status} - ${statusText}`
-    );
-    this.name = 'GitHubCommitFetchError';
-    Object.setPrototypeOf(this, GitHubCommitFetchError.prototype);
-  }
-}
-
-/**
  * A data getter that fetches the time series data from the JHU CSSE GitHub repository.
  *
  * For more information about its methods see {@link DataGetter}.
@@ -47,55 +20,41 @@ export class GitHubGetter implements DataGetter {
     const rawResponse = await fetch(url);
 
     if (!rawResponse.ok) {
-      throw new GitHubDataFetchError(rawResponse.status, rawResponse.statusText);
+      throw new DataGetterError(
+        `There was an error fetching the data from GitHub. Response status: ${rawResponse.status} - ${rawResponse.statusText}`
+      );
     }
 
     return await rawResponse.text();
   }
 
-  /**
-   * @throws {@link GitHubDataFetchError} when there is an error fetching the data.
-   */
   async getGlobalConfirmedData(): Promise<string> {
     return await GitHubGetter.fetchData(this.globalConfirmedUrl);
   }
 
-  /**
-   * @throws {@link GitHubDataFetchError} when there is an error fetching the data.
-   */
   async getGlobalDeathsData(): Promise<string> {
     return await GitHubGetter.fetchData(this.globalDeathsUrl);
   }
 
-  /**
-   * @throws {@link GitHubDataFetchError} when there is an error fetching the data.
-   */
   async getGlobalRecoveredData(): Promise<string> {
     return await GitHubGetter.fetchData(this.globalRecoveredUrl);
   }
 
-  /**
-   * @throws {@link GitHubDataFetchError} when there is an error fetching the data.
-   */
   async getUSConfirmedData(): Promise<string> {
     return await GitHubGetter.fetchData(this.usConfirmedUrl);
   }
 
-  /**
-   * @throws {@link GitHubDataFetchError} when there is an error fetching the data.
-   */
   async getUSDeathsData(): Promise<string> {
     return await GitHubGetter.fetchData(this.usDeathsUrl);
   }
 
-  /**
-   * @throws {@link GitHubCommitFetchError} when there is an error fetching the commit date.
-   */
   async getSourceLastUpdatedAt(): Promise<Date | undefined> {
     const response = await fetch(this.commitDataUrl);
 
     if (!response.ok) {
-      throw new GitHubCommitFetchError(response.status, response.statusText);
+      throw new DataGetterError(
+        `There was an error fetching the commit date from the GitHub API. Response status: ${response.status} - ${response.statusText}`
+      );
     }
 
     const json = await response.json();
